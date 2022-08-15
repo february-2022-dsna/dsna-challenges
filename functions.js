ed95eec2729f08377853556ed6c0a2ffc0f44023
Kevin Roney <ktroney.pdx@gmail.com>
2022-08-15 16:45:17 -0700

    added reverse words

--- functions.js
+++ functions.js
@@ -48,4 +48,27 @@ function titleCase(sentence) {
     .join(" ");//join the sentence back together
 }
 
-module.exports = { anagrams, oddishOrEvenish, uniqueString, titleCase };
+function reverseWords(sentence) {
+  //takes in a sentence of strings
+  //reverses the order of letters for each word, but words remain in same order
+  //returns reversed sentence
+
+  const newSentence = sentence
+    //splits sentence into individual words
+    .split(" ")
+    //reverses order of words
+    .reverse()
+    //joins words together in string
+    .join(" ");
+
+  return newSentence
+    //splits string into individual characters
+    .split("")
+    //reverses order of characters
+    .reverse()
+    //joins them back together
+    .join("");
+}
+
+
+module.exports = { anagrams, oddishOrEvenish, uniqueString, titleCase, reverseWords };

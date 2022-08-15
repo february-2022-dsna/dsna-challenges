b04b62c422562c38cc4b0fe6a7af6309bfe29cec
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-15 10:37:30 -0700

    removed pop func

--- day-01/arrays.test.js
+++ day-01/arrays.test.js
@@ -5,7 +5,7 @@ function push(arr, item) {
 
 // TIME COMPLEXITY - O(1) -- no need to shift array, adding to end
 
-test("push array", () => {
+test("add item to end of array", () => {
   const arr = ["a", "b", "c"];
   const newLength = push(arr, "d");
   expect(arr).toEqual(["a", "b", "c", "d"]);
@@ -24,7 +24,7 @@ function unshift(arr, item) {
 
 // TIME COMPLEXITY - O(n) -- inserting requires shifting elements to the right by one
 
-test("unshift array", () => {
+test("adjust items in array to make room and add item to beginning of array", () => {
   const arr = ["a", "b", "c"];
   const newLength = unshift(arr, "d");
   expect(arr).toEqual(["d", "a", "b", "c"]);

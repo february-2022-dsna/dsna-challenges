8b57e9ea61dcce13959e17928b6a4da0e72e1bad
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-08-15 15:56:02 -0700

    unique string passing

--- day-01/sandbox.test.js
+++ day-01/sandbox.test.js
@@ -6,9 +6,54 @@ function annagram(word1, word2){
 
 test('time to test a function', () => {
 
-    const expected = annagram('superintended','unpredestined');
+    const expected = true;
     
-    const actual = true;
+    const actual = annagram('superintended','unpredestined');
 
     expect(expected).toEqual(actual);
+});
+
+
+// function removeDuplicate(string)
+// {
+//    return string.split('')
+//     .filter(function(item, pos, self)
+//     {
+//       return self.indexOf(item) == pos;
+//     }
+//    ).join('');
+// }
+
+function uniqueString(booger) {
+
+    const newBoog = booger.map(str => { return [...new Set(str.toLowerCase())].sort().join('')});
+    for ( let i = 0; i < newBoog.length; i++ ) {
+        if ( newBoog.indexOf(newBoog[i]) === newBoog.lastIndexOf(newBoog[i]) ) return booger[i]
+    }
+}
+    
+
+
+
+
+
+
+
+test('uniqe string', () => {
+
+    const strings = [ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ];
+    
+    const expected = 'BbBb';
+
+    const actual = uniqueString(strings);
+
+    expect(expected).toEqual(actual);
+
+    const strings2 = [ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ];
+    
+    const expected2 = 'foo';
+
+    const actual2 = uniqueString(strings2);
+
+    expect(expected2).toEqual(actual2);
 });

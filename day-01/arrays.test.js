0b6a8044c05d6a17f93551c1f6c8730f9922d79a
Beau Elliott <belliott1516@gmail.com>
2022-08-15 16:23:41 -0700

    completed hasDuplicates function

--- day-01/arrays.test.js
+++ day-01/arrays.test.js
@@ -3,6 +3,13 @@ function push(arr, item){
     return arr;
 }
 
+test('array push', () => {
+    const arr = ['a', 'b', 'c'];
+    const actual = push(arr, 'd');
+    expect(actual).toEqual(['a', 'b', 'c', 'd']);
+    expect(actual.length).toBe(4);
+});
+
 function unshift(arr, item){ 
    for(let i = arr.length -1; i >= 0; i--){
     arr[i +1] = arr[i];
@@ -11,11 +18,26 @@ function unshift(arr, item){
     return arr;
 }
 
+test('array unshift', () => {
+    const arr = ['a', 'b', 'c'];
+    const actual = unshift(arr, 'd');
+    expect(actual).toEqual(['d', 'a', 'b', 'c']);
+    expect(actual.length).toBe(4);
+});
+
 function pop(arr){
     if(arr.length === 0) return null;
     arr.length = arr.length -1;
     return arr;
 }
+
+test('array pop', () => {
+    const arr = ['a', 'b', 'c'];
+    const actual = pop(arr);
+    expect(actual).toEqual(['a', 'b',]);
+    expect(actual.length).toBe(2);
+});
+
 
 function shift(arr){
     if (arr.length === 0) return null;
@@ -27,40 +49,41 @@ function shift(arr){
     return arr;
 }
 
+test('array shift', () => {
+    const arr = ['a', 'b', 'c'];
+    const actual = shift(arr);
+    expect(actual).toEqual(['b', 'c',]);
+    expect(actual.length).toBe(2);
+});
+
+function hasDuplicates(arr){
+    //edge case in case arr is empty
+    if(arr.length === 0) return null;
+    // sort arr decending placing repetitive items next to each other
+    const newArr = arr.sort();
+    //loop through array
+    for(let i = 0; i < newArr.length; i++) {
+        //if element matches next element return true
+       if(newArr[i] === newArr[i+1]) return true;
+    }
+    //if no elements match return false
+    return false;
+}
+
+test('array hasDuplicates', () => {
+    const arr = ['y', 'k', 'a', 'b', 'c', 'f'];
+    const arr2 = ['a', 'b', 'e', 'd', 'e', 'f'];
+    const first = hasDuplicates(arr);
+    expect(first).toEqual(false);
+    const second = hasDuplicates(arr2);
+    expect(second).toEqual(true);
+});
+
 function inventoryChecker(inventory, { itemName }){
     //look through inventory and compare with item
     // if item has quantity greater than 5 give string else give other string
     return inventory[itemName] > 5 ? 'plenty of inventory' : `running low on ${itemName}`
 }
-
-test('array push', () => {
-    const arr = ['a', 'b', 'c'];
-    const actual = push(arr, 'd');
-    expect(actual).toEqual(['a', 'b', 'c', 'd']);
-    expect(actual.length).toBe(4);
-});
-
-test('array unshift', () => {
-    const arr = ['a', 'b', 'c'];
-    const actual = unshift(arr, 'd');
-    expect(actual).toEqual(['d', 'a', 'b', 'c']);
-    expect(actual.length).toBe(4);
-});
-
-test('array pop', () => {
-    const arr = ['a', 'b', 'c'];
-    const actual = pop(arr);
-    expect(actual).toEqual(['a', 'b',]);
-    expect(actual.length).toBe(2);
-});
-
-test('array shift', () => {
-    const arr = ['a', 'b', 'c'];
-    const actual = shift(arr);
-    console.log('actual', actual);
-    expect(actual).toEqual(['b', 'c',]);
-    expect(actual.length).toBe(2);
-});
 
 test('inventory checker', () => {
     const inventory = { eggs: 20, bananas: 3, grapes: 12 };
@@ -69,4 +92,11 @@ test('inventory checker', () => {
     expect(checker).toEqual('plenty of inventory');
     expect(inventoryChecker(inventory, { itemName: 'bananas'})).toBe('running low on bananas')
 });
+
+
+
+
+
+
+
 

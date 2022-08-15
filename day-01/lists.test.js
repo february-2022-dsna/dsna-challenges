d4b6ac99b4011d5fdbe0ee7ec4696733da10a276
Beau Elliott <belliott1516@gmail.com>
2022-08-15 16:42:54 -0700

    completed multiples of n in lists test

--- day-01/lists.test.js
+++ day-01/lists.test.js
@@ -1,3 +1,20 @@
+function multiplesOfN(n){
+  //set an empty array
+  const list = [];
+  //loop through 50 instances and push only those that are modulus of n
+  for(let i = 1; i <= 50; i++){
+    if(i % n === 0)list.push(i);
+  }
+  return list;
+}
+
+test('multiplesOfN Test', () => {
+  expect(multiplesOfN(5)).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+  expect(multiplesOfN(25)).toEqual([25, 50]);
+  expect(multiplesOfN(10)).toEqual([10, 20, 30, 40, 50]);
+});
+
+
 function fizzBuzz(n){
     const fizzBuzzArr = Array.from({ length: n }, (_, i) => i + 1); 
   for(let i = 1; i < fizzBuzzArr.length; i++){

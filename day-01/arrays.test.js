cd56dcb4d8de4d16627b97c549b55646b8682bde
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-15 11:03:55 -0700

    added objects.test for inventoryChecker func

--- day-01/arrays.test.js
+++ day-01/arrays.test.js
@@ -5,10 +5,10 @@ function push(arr, item) {
 
 // TIME COMPLEXITY - O(1) -- no need to shift array, adding to end
 
-test("add item to end of array", () => {
-  const arr = ["a", "b", "c"];
-  const newLength = push(arr, "d");
-  expect(arr).toEqual(["a", "b", "c", "d"]);
+test('add item to end of array', () => {
+  const arr = ['a', 'b', 'c'];
+  const newLength = push(arr, 'd');
+  expect(arr).toEqual(['a', 'b', 'c', 'd']);
   expect(newLength).toBe(4);
 });
 
@@ -24,10 +24,10 @@ function unshift(arr, item) {
 
 // TIME COMPLEXITY - O(n) -- inserting requires shifting elements to the right by one
 
-test("adjust items in array to make room and add item to beginning of array", () => {
-  const arr = ["a", "b", "c"];
-  const newLength = unshift(arr, "d");
-  expect(arr).toEqual(["d", "a", "b", "c"]);
+test('adjust items in array to make room and add item to beginning of array', () => {
+  const arr = ['a', 'b', 'c'];
+  const newLength = unshift(arr, 'd');
+  expect(arr).toEqual(['d', 'a', 'b', 'c']);
   expect(newLength).toBe(4);
 });
 

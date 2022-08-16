df0a6b4ad7144cf62cea01677b347c60d1276408
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-16 10:40:33 -0700

    wrote and passed test for filter func

--- day-02/array-methods.test.js
+++ day-02/array-methods.test.js
@@ -16,3 +16,25 @@ test('maps through an array', () => {
   const actual = map(arr, (n) => n ** 2);
   expect(actual).toEqual([1, 36, 25]);
 });
+
+// ------------------------------ 2 - FILTER -----------------
+
+function filter(arr, predicate) {
+  const mapped = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      mapped.push(arr[i]);
+    }
+  }
+
+  return mapped;
+}
+
+//TIME COMPLEXITY -
+
+test.only('filters through an array', () => {
+  const arr = [2, 6, 5];
+  const actual = filter(arr, (n) => n % 2 === 0);
+  expect(actual).toEqual([2, 6]);
+});

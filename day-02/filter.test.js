2b7c46417c78b05cbaf8080a804a5671b5622f83
Cody-Wise <cody@sharptackmedia.com>
2022-08-16 15:46:36 -0700

    Updated filter function and test

--- day-02/filter.test.js
+++ day-02/filter.test.js
@@ -1,11 +1,14 @@
-function filter(arr) {
-  return arr.filter((item) => {
-    return item % 2 === 0;
-  });
+function filter(arr, predicate) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (predicate(item)) result.push(item);
+  }
+  return result;
 }
 
 test('filter func', () => {
   const expected = [2, 4, 6, 8];
-  const actual = filter([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  expect(expected).toEqual(actual);
+  const actual = filter(expected, (n) => n % 2 === 0);
+  expect(actual).toEqual(expected);
 });

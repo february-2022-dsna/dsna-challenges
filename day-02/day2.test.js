fe846309321acb1bb24edf8c5c165d83a4064e4a
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-08-16 16:29:13 -0700

    got fib passing

--- day-02/day2.test.js
+++ day-02/day2.test.js
@@ -83,7 +83,7 @@ function every(arr, predicate) {
     return true;
 }
 
-test('root digit', () => {
+test('every', () => {
 
     const expected = true;
 
@@ -99,3 +99,43 @@ test('root digit', () => {
 
 
 });
+
+test('fib', () => {
+
+    const expected = 0;
+    const actual = fib(0);
+
+    const expected2 = 1;
+    const actual2 = fib(1);
+
+    const expected3 = 1;
+    const actual3 = fib(2);
+
+    const expected4 = 8;
+    const actual4 = fib(6);
+
+    const expected5 = 21;
+    const actual5 = fib(8);
+
+    expect(actual).toEqual(expected);
+    expect(actual2).toEqual(expected2);
+    expect(actual3).toEqual(expected3);
+    expect(actual4).toEqual(expected4);
+    expect(actual5).toEqual(expected5);
+
+
+});
+
+let F = [0,1]
+
+function fib(n) {
+    
+    if(F.length > n) return F[n]
+    
+    F[F.length] = F[F.length - 2] + F[F.length - 1]
+    
+    return fib(n);
+
+
+    
+}

49e2c255ad98aac6fe86ad20e093cd81fbb44b4f
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-08-16 15:31:46 -0700

    every passing

--- day-02/day2.test.js
+++ day-02/day2.test.js
@@ -27,4 +27,75 @@ test('add first', () => {
 
     expect(actual).toEqual(expected);
 
+
+    const addCat = addFirst('cat')
+
+    const expected2 = ['cat','dog','bird','lizard'];
+
+    const actual2 = addCat(['dog', 'bird', 'lizard']);
+
+    expect(actual2).toEqual(expected2);
+
+});
+
+function rootDigit(n) {
+   
+    if(n.toString().length === 1){ 
+        return n;
+    }
+
+    const digits = n.toString().split('');
+    const numbers = digits.map(d => +d);
+    const sum = numbers.reduce((a, b) => a + b);
+    
+    return rootDigit(sum);
+
+}
+
+test('root digit', () => {
+
+    const expected = 6;
+
+    const actual = rootDigit(123);
+
+    expect(actual).toEqual(expected);
+
+    const expected2 = 2;
+
+    const actual2 = rootDigit(4322);
+
+    expect(actual2).toEqual(expected2);
+
+    const expected3 = 9;
+
+    const actual3 = rootDigit(999888777);
+
+    expect(actual3).toEqual(expected3);
+
+});
+
+function every(arr, predicate) {
+
+    for(let i = 0; i < arr.length; i++) {
+        if(predicate(arr[i]) === false) return false;
+    }
+    
+    return true;
+}
+
+test('root digit', () => {
+
+    const expected = true;
+
+    const actual = every([2, 4, 6], n => n % 2 === 0);
+
+    expect(actual).toEqual(expected);
+
+    const expected2 = false;
+
+    const actual2 = every([1, 2, 3], n => n % 2 === 0);
+
+    expect(actual2).toEqual(expected2);
+
+
 });

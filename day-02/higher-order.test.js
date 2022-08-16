40c165b1f54adf71d811f5b1fc392e3696fb51e1
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-16 14:57:34 -0700

    wrote and passed test for addFirst func

--- day-02/higher-order.test.js
+++ day-02/higher-order.test.js
@@ -4,10 +4,24 @@ const addPunctuation = (punctuation) => (phrase) => phrase + punctuation;
 
 //TIME COMPLEXITY -
 
-test.only('add punctuation', () => {
+test('add punctuation', () => {
   const addExcitement = addPunctuation('!!!');
   expect(addExcitement('Hello World')).toBe('Hello World!!!');
 
   const addUnsure = addPunctuation('?!?');
   expect(addUnsure('Hello World')).toBe('Hello World?!?');
+});
+
+// ------------------------------ 5 - ADD FIRST ELEMENT -----------------
+
+const addFirst = (element) => (array) => Array(element).concat(array);
+
+test('add first element', () => {
+  const addOrange = addFirst('orange');
+  expect(addOrange(['red', 'blue', 'green'])).toEqual([
+    'orange',
+    'red',
+    'blue',
+    'green',
+  ]);
 });

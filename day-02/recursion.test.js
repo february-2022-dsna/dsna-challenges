381bf89fe336fe18e0d4a79bf79931ec5e56019a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-16 15:48:10 -0700

    added time complexity for rootdigit func

--- day-02/recursion.test.js
+++ day-02/recursion.test.js
@@ -1,11 +1,11 @@
 // ------------------------------ 1 - DIGIT SUM ROOT -----------------
 
 function rootDigit(n) {
-  const digits = n.toString().split('');
+  const digits = n.toString().split(''); // O(n)
   console.log('digits', digits);
-  const numbers = digits.map((d) => +d);
+  const numbers = digits.map((d) => +d); // O(n)
   console.log('numbers', numbers);
-  const sum = numbers.reduce((a, b) => a + b);
+  const sum = numbers.reduce((a, b) => a + b); // O(n)
   console.log('sum', sum);
 
   return sum.toString().length === 1 ? sum : rootDigit(sum);
@@ -15,7 +15,7 @@ function rootDigit(n) {
   //   return rootDigit(sum);
 }
 
-//TIME COMPLEXITY -
+//TIME COMPLEXITY - O(n)
 
 test.only('digits sum root', () => {
   const firstOutput = rootDigit(123);

cfae880339be586b7a8468e552dc81894b5e28c2
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-08-18 14:23:47 -0700

    linkedlistnode passing

--- day-03/node.test.js
+++ day-03/node.test.js
@@ -1,47 +1,19 @@
-const node3 = {
-    value: 4,
-    next: null,
-  };
+// const node3 = {
+//     value: 4,
+//     next: null,
+//   };
   
-  const node2 = {
-    value: 12,
-    next: node3,
-  };
+//   const node2 = {
+//     value: 12,
+//     next: node3,
+//   };
   
-  const node1 = {
-    value: 34,
-    next: node2,
-  };
+//   const node1 = {
+//     value: 34,
+//     next: node2,
+//   };
   
-  class LinkedListNode {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
   
-    add(node) {
-      if (!this.next) this.next = node;
-      else {
-        this.next.add(node);
-      }
-    }
-  }
-  
-  test('linked list adds', () => {
-    const root = new LinkedListNode('A');
-    const nodeB = new LinkedListNode('B');
-    const nodeC = new LinkedListNode('C');
-    const nodeD = new LinkedListNode('D');
-
-    root.add(nodeB);
-    root.add(nodeC);
-    root.add(nodeD);
-  
-    expect(root.next).toBe(nodeB);
-    expect(nodeB.next).toBe(nodeC);
-    expect(nodeC.next).toBe(nodeD);
-    expect(nodeD.next).toBe(null);
-  });
   
   // console.log(root.getList()); // 'A B'
   // const nodeC = new LinkedListNode('C');
@@ -67,9 +39,11 @@ const node3 = {
             if(!this.right) {this.right = node} else {this.right.add(node)};
         }
     }
+
   }
 
-  test.only('binary tree', () => {
+  test('binary tree', () => {
+
     const B = new BinaryTreeNode(2);
     const A = new BinaryTreeNode(1);
     const C = new BinaryTreeNode(3);
@@ -86,12 +60,44 @@ const node3 = {
     expect(A.left).toBe(null);
     expect(C.left).toBe(null);
   });
-//   class TreeNode {
-//     constructor(value) {
-//       this.value = value;
-//       this.children = [];
-//     }
+
+  test.only('linked list', () => {
+    
+    const root = new LinkedListNode('A');
+    const nodeB = new LinkedListNode('B');
+    root.add(nodeB);
+    console.log(root.getList()); // 'A B'
+    const nodeC = new LinkedListNode('C');
+    const nodeD = new LinkedListNode('D');
+    const nodeE = new LinkedListNode('E');
+    root.add(nodeC);
+    root.add(nodeD);
+    root.add(nodeE);
+    console.log(root.getList()); // 'A B C D E'
+    // root.remove(1);
+    console.log(root.getList()); // 'A C D E'
+
+    expect(root.next).toBe(nodeB);
+    expect(nodeB.next).toBe(nodeC);
+    expect(nodeC.next).toBe(nodeD);
+    expect(nodeD.next.next).toBe(null);
+  });
+
+  class LinkedListNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
   
-//     add(node) {}
-//   }
-  
+    add(node) {
+      if (!this.next) this.next = node;
+      else {
+        this.next.add(node);
+      }
+    }
+
+    getList(node) {
+        if(!this.next) return this.value;
+        return `${this.value} ${this.next.getList()}`;
+    }
+}

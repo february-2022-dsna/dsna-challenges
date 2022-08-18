f7960bced71e9ddef3b4f196827e70e07f331086
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-18 14:28:08 -0700

    wrote and passed test for adding node to linked list

--- day-03/nodes/nodes.test.js
+++ day-03/nodes/nodes.test.js
@@ -25,7 +25,7 @@ class BinaryTreeNode {
   }
 }
 
-test.only('binary add node', () => {
+test('binary add node', () => {
   const B = new BinaryTreeNode('B');
   const A = new BinaryTreeNode('A');
   const C = new BinaryTreeNode('C');
@@ -35,4 +35,31 @@ test.only('binary add node', () => {
   B.add(C);
   expect(B.left).toBe(A);
   expect(B.right).toBe(D);
+});
+
+// ------------------------------ 2 - LINKED LIST -----------------
+
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  add(node) {
+    if (!this.next) this.next = node;
+    else {
+      this.next.add(node);
+    }
+  }
+}
+
+test.only('adding node to linked list', () => {
+  const root = new LinkedListNode('A');
+  const nodeB = new LinkedListNode('B');
+  const nodeC = new LinkedListNode('C');
+  const nodeD = new LinkedListNode('D');
+  root.add(nodeB);
+  root.add(nodeC);
+  root.add(nodeD);
+  expect(root.next).toBe(nodeB);
 });
